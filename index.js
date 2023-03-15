caa6c6410cf150906c493df998b459d0ddd88dce
unknown <sashardoval@gmail.com>
2023-03-15 15:37:46 -0700

    commpleted start functions

--- index.js
+++ index.js
@@ -29,6 +29,23 @@ const questions = [
 
 
     //if add role:
+    // {
+    //     type: 'list',
+    //     message: 'What department does this role belong to?',
+    //     choices: deptNamesArray,
+    //     name: 'assignDept',
+    // },
+    // {
+
+    //     type: 'input',
+    //     message: 'What is the name of the role?',
+    //     name: 'newRole',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What is the salary for this role?',
+    //     name: 'addSalary',
+    // }
 
 
     //if add employee:
@@ -95,7 +112,7 @@ async function viewEmployees() {
 
     init()
 }
-//add data
+//functions that add data
 async function addDept() {
     const questions = await inquirer.prompt([{
 
@@ -106,45 +123,98 @@ async function addDept() {
     }])
     if (questions) {
         const results = db.promise().query('INSERT INTO department(dept_names) VALUES (?)', questions.addDept)
-        console.table(results)
+        console.table(results[0])
         console.log('Deptartment added!')
     }
     init()
 }
 
-// async function addRole() {
+//HEY SASH IT'S ME SASH HERE'S THE PROBLEM CHILD I SAVED FOR YOU PLEASE FIX IT LOVE YOU BYEEE
+async function addRole() {
+    //create array of dept name for users to select in inquirer CURRENTLY RETURNS UNDEFINED
+    let deptNamesArray = [];
+    //supposedly pushes dept names into array
+    const dept = await db.promise().query('SELECT dept_names FROM department');
+    deptNamesArray.push(dept[0]);
 
-//     // const roleChoices = async () => {
-//     //     const departments = await db.query('SELECT dept_names FROM department;');
-//     //     return departments[0];
-//     // };
-//     const questions = await inquirer.prompt([
-//         {
+    const questions = await inquirer.prompt([
+        //put questions here
+        {
+            type: 'input',
+            message: 'What is the name of the role?',
+            name: 'newRole',
+        },
+        {
+            type: 'input',
+            message: 'What is the salary for this role?',
+            name: 'addSalary',
+        },
+        {
+            type: 'list',
+            message: 'What department does this role belong to?',
+            choices: deptNamesArray,
+            name: 'assignDept',
+        }
+    ])
 
-//             type: 'input',
-//             message: 'What is the name of the role?',
-//             name: 'newRole',
-//         },
-//         {
-//             type: 'input',
-//             message: 'What is the salary for this role?',
-//             name: 'addSalary',
-//         },
-//         // {
-//         //     type: 'list',
-//         //     message: 'What department does this role belong to?',
-//         //     choices: SELECT dept_names FROM department,
-//         //     name: 'assignDept',
-//         // }
+    if (questions) {
+        const results = db.promise().query('INSERT INTO roles(title, salary, dept_id) VALUES (?)', questions.newRole, questions.addSalary, questions.addDept)
+        console.table(results)
+        
+        console.log(deptNamesArray);
+    }
+    init()
+}
 
-//     ])
-//     if (questions) {
-//         const results = db.promise().query('INSERT INTO roles(title, salary, dept_id) VALUES (?)', questions.newRole, questions.addSalary, questions.assignDept)
-//         console.table(results)
-//         console.log('role added!')
-//     }
-//     init()
-// }
+async function addEmployee() {
+    //create array of dept name for users to select in inquirer CURRENTLY RETURNS UNDEFINED
+    let roleNamesArray = [];
+    let employeeNamesArray = [];
+    //supposedly pushes roles and names into respective arrays
+    const roles = await db.promise().query('SELECT title FROM roles');
+    const names = await db.promise().query('SELECT (first_name, last_name) FROM employees');
+
+    roleNamesArray.push(roles[0]);
+    employeeNamesArray.push(names[0]);
+
+    const questions = await inquirer.prompt([
+        //put questions here
+        {
+            type: 'input',
+            message: "What is the new Employee's first name?",
+            name: 'firstName',
+        },
+        {
+            type: 'input',
+            message: "What is the new Employee's last name?",
+            name: 'lastName',
+        },
+
+        //     {
+        //         type: 'list',
+        //         message: "What is this employee's role?",
+        //         choices: [list existing roles],
+        //         name: 'assignRole',
+        //     },
+        // {
+        //     type: 'list',
+        //     message: "Who is this employee's manager?",
+        //     choices: [list employee names],
+        //     name: 'assignManager',
+        // }
+    ])
+
+    if (questions) {
+        const results = db.promise().query('INSERT INTO employees(first_name, last_name,) VALUES (?)', questions.firstname, questions.lastname)
+        console.table(results[0])
+        
+        console.log(deptNamesArray);
+    }
+    init()
+}
+
+async function updateRole()
+
 
 function init() {
     inquirer.prompt([{
@@ -157,7 +227,7 @@ function init() {
             'add a role',
             'add an employee',
             'update an employee role',
-            '],
+            'exit'],
         name: 'start'
     },])
         .then((response) => {
@@ -166,11 +236,11 @@ function init() {
                 viewDepartments();
             }
 
-            if(response.start === "view all roles"){
+            if (response.start === "view all roles") {
                 viewRoles();
             }
 
-            if(response.start === "view all employees"){
+            if (response.start === "view all employees") {
                 viewEmployees();
             }
 
@@ -180,6 +250,15 @@ function init() {
 
             if (response.start === "add a role") {
                 addRole();
+            }
+            if (response.start === "add an employee") {
+                addEmployee();
+            }
+            if (response.start === "update an employee role") {
+                updateRole();
+            }
+            if (response.start === 'exit') {
+                console.log("Bye for now");
             }
         });
 };
